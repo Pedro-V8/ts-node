9a09778b23b51231f209302b3535e5775f09bbc0
Pedro-V8 <pedrophdv@outlook.com>
2021-11-03 10:51:47 -0300

    Adding conditinos on posting Products and initializing update product controller

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -2,12 +2,33 @@ import { getRepository } from "typeorm";
 import { Product } from "../entity/Product";
 import { Request , Response } from 'express';
 
+type Produto = {
+    name: String,
+    model: String,
+    quantity: String,
+}
+
 export const getProducts = async(req: Request , res: Response) => {
     const products = await getRepository(Product).find()
     return res.json(products)
 }
 
 export const postProducts = async(req: Request , res: Response) => {
-    const product = await getRepository(Product).save(req.body)
-    return res.json(product)
+    const prod: Produto = req.body
+    try {
+        if(prod.name === '' || prod.model === ''){
+            return res.status(400).send({ error: 'Empty Fields' })
+        }else if(+prod.quantity < 0){
+            return res.status(400).send({ error: 'Wrong Quantity' })
+        }else{
+            const product = await getRepository(Product).save(req.body)
+            return res.json(product)
+        }
+    } catch (error) {
+        console.log(prod.name)
+        return res.status(400).send({ error: 'Error Registration' })
+    }
+
 }
+
+export const updateProducts = async(req: Request , res: Response) => {}
